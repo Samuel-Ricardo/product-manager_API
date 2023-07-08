978f125e1b9b8d129f4048fd46de34fb06fee989
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-08 19:25:11 -0300

    [:sparkles:]  | create: resource (Products)

--- product_manager/src/app.module.ts
+++ product_manager/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoryModule } from './category/category.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [CategoryModule, PrismaModule],
+  imports: [CategoryModule, PrismaModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
